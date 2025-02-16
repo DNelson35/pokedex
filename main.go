8790058142beb68584aac1ca544f4758df5c8e47
Damien Nelson <dhnisaman@gmail.com>
2025-02-16 18:35:51 -0500

    simple refactor

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+
 
 func main(){
-	scanner := bufio.NewScanner(os.Stdin)
-	
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan(){
-			input := scanner.Text()
-			result := cleanInput(input)
-			if len(result) <= 0 {
-				fmt.Println("no input detected")
-			} else {
-				fmt.Printf("Your command was: %v\n", result[0])
-			}
-		}
-	}
+	startRepl()
 }
 
--- repl.go
+++ repl.go
@@ -1,7 +1,13 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
 
+// strings.Fields would have gave the same result
 func cleanInput(txt string) []string {
 	var wordArr []string
 	if len(txt) == 0 {
@@ -17,4 +23,19 @@ func cleanInput(txt string) []string {
 
 	return wordArr
 }
-// strings.Fields would have gave the same result
+
+func startRepl(){
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan(){
+			result := cleanInput(scanner.Text())
+			if len(result) == 0 {
+				fmt.Println("no input detected")
+			} else {
+				fmt.Printf("Your command was: %v\n", result[0])
+			}
+		}
+	}
+}
